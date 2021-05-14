9a5fe11aca31b519d5f07a5fe3997d914e8c1ceb
Graham David Bryant <gdb@roamingsolutions.net>
2021-05-14 17:06:01 +0200

    fixed yaml format and started convert.py

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
-import json
+# import json
 import logging
 import os
 import sys
 import yaml
 import fnmatch
-from typing import List, TextIO
+from typing import List #, TextIO
 
 
 def main() -> None:
@@ -16,21 +16,19 @@ def main() -> None:
         level=logging.INFO,
     )
     args = parse_arguments(sys.argv[1:])
-    
+
     yaml_files = []
-    for root, dirnames, filenames in os.walk('../source'):
-        if args.debug:
-            print("--- root = " + str(root))
-            for name in filenames:
-                print("--- files = " + str(os.path.join(root,name)))
-        for filename in fnmatch.filter(filenames, '*.yaml'):
+    for root, dirnames, filenames in os.walk("../source"):
+        for filename in fnmatch.filter(filenames, "*.yaml"):
             yaml_files.append(os.path.join(root, filename))
-    if args.debug: print("--- yaml_files = " + str(yaml_files))
+    # if args.debug: print("--- yaml_files = " + str(yaml_files))
     
-    if args.output != None and len(yaml_files) > 0:
+    if len(yaml_files) > 0:
         for file in yaml_files:
-            yaml.load(file)
-            if args.debug: print('--- file = ' + str(file))
+            # if args.debug: print("--- file = " + str(file))
+            with open(file, "r") as f:
+                data = yaml.safe_load(f)
+            if args.debug: print("--- loaded data successfully. " + str(file) + " with " + str(len(data)) + " keys: " + str(data.keys()))
 
 
 def parse_arguments(args: List[str]) -> argparse.Namespace:
@@ -40,7 +38,7 @@ def parse_arguments(args: List[str]) -> argparse.Namespace:
         "-o",
         "--output",
         type=str,
-        choices=["json", "pdf", "indd"],
+        choices=["json", "pdf", "indd", "docx"],
         default=None,
         help="Output format to produce.",
     )
@@ -51,6 +49,8 @@ def parse_arguments(args: List[str]) -> argparse.Namespace:
         help="Output additional information to debug script",
         )
     return parser.parse_args(args)
+
+
 
 
 if __name__ == "__main__":
