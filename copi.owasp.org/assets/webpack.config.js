137ab2a009378d38be9b7f664b85940ca0b724aa
Toby Irvine <toby@securedelivery.io>
2021-05-22 19:00:52 +0100

    update the old dependencies in the front-end webpack stuff and fix the resulting breakages

--- copi.owasp.org/assets/webpack.config.js
+++ copi.owasp.org/assets/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
@@ -12,8 +11,8 @@ module.exports = (env, options) => {
   return {
     optimization: {
       minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
+        new TerserPlugin({ parallel: true }),
+        new CssMinimizerPlugin()
       ]
     },
     entry: {
@@ -46,8 +45,7 @@ module.exports = (env, options) => {
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+      new CopyWebpackPlugin({ patterns: [{ from: 'static/', to: '../' }]})
     ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
   }
 };
