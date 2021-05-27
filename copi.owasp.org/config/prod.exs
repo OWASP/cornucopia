dd9baab20901e33378427a1f937986a007cf0062
Toby Irvine <toby@securedelivery.io>
2021-05-27 13:12:59 +0100

    updating hostname to copi.securedelivery.io

--- copi.owasp.org/config/prod.exs
+++ copi.owasp.org/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :copi, CopiWeb.Endpoint,
-  url: [host: "copi.gigalixirapp.com", port: 443],
+  url: [host: "copi.securedelivery.io", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
