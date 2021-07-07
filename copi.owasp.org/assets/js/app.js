780a50dbc6d912d3c1e4b845fbc8bfea6022a547
Toby Irvine <toby@securedelivery.io>
2021-07-07 11:56:08 +0100

    prevent cards being unplayed in a round, for now

--- copi.owasp.org/assets/js/app.js
+++ copi.owasp.org/assets/js/app.js
@@ -35,8 +35,11 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
-dragula([document.querySelector('#hand'), document.querySelector('#table')])
-.on('drop', (element, target) => {
+dragula([document.querySelector('#hand'), document.querySelector('#table')], {
+    accepts: function (el, target) {
+        return target !== document.getElementById('hand')
+    }
+}).on('drop', (element, target) => {
     fetch('/api/games/' + element.dataset.game + '/players/' + element.dataset.player + '/card', {
       method: 'PUT',
       headers: {
