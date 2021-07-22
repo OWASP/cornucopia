b2466c18ad050bde52c6fbf92534c500929245f7
GDBryant <gdb@roamingsolutions.net>
2021-07-22 18:10:32 +0200

    Tests working

--- test/scripts/convert_utest.py
+++ test/scripts/convert_utest.py
@@ -2,8 +2,9 @@ import unittest
 import argparse
 import os
 import docx
+import logging
 
-from scripts.convert import Convert as c
+from scripts.convert import Convert
 
 
 class TestParseArguments(unittest.TestCase):
@@ -17,7 +18,7 @@ class TestParseArguments(unittest.TestCase):
             debug=False,
         )
 
-        got_args = c.parse_arguments(c, input_args)
+        got_args = Convert.parse_arguments(Convert, input_args)
         self.maxDiff = None
         self.assertEqual(got_args, want_args)
 
@@ -31,7 +32,7 @@ class TestParseArguments(unittest.TestCase):
             debug=False,
         )
 
-        got_args = c.parse_arguments(c, input_args)
+        got_args = Convert.parse_arguments(Convert, input_args)
         self.maxDiff = None
         self.assertEqual(got_args, want_args)
 
@@ -45,7 +46,7 @@ class TestParseArguments(unittest.TestCase):
             debug=False,
         )
 
-        got_args = c.parse_arguments(c, input_args)
+        got_args = Convert.parse_arguments(Convert, input_args)
         self.maxDiff = None
         self.assertEqual(got_args, want_args)
 
@@ -59,75 +60,149 @@ class TestParseArguments(unittest.TestCase):
             debug=False,
         )
 
-        got_args = c.parse_arguments(c, input_args)
+        got_args = Convert.parse_arguments(Convert, input_args)
         self.maxDiff = None
         self.assertEqual(got_args, want_args)
 
 
 class TestMakeTemplate(unittest.TestCase):
     def test_make_template_true(self) -> None:
-        c.args = argparse.Namespace(language="template")
+        Convert.args = argparse.Namespace(language="template")
 
-        result = c.make_template(c)
+        result = Convert.make_template(Convert)
         self.assertTrue(result)
 
     def test_make_template_false(self) -> None:
-        c.args = argparse.Namespace(language="en")
+        Convert.args = argparse.Namespace(language="en")
 
-        result = c.make_template(c)
+        result = Convert.make_template(Convert)
         self.assertFalse(result)
 
     def test_make_template_empty(self) -> None:
-        c.args = argparse.Namespace()
+        Convert.args = argparse.Namespace()
 
-        result = c.make_template(c)
+        result = Convert.make_template(Convert)
         self.assertFalse(result)
 
 
 class TestGetFilesFromOfType(unittest.TestCase):
     def test_get_files_from_of_type_source_yaml_files(self) -> None:
-        c.args = argparse.Namespace(debug=False)
-        path = c.BASE_PATH + "/test/test_files/"
+        Convert.args = argparse.Namespace(debug=False)
+        path = Convert.BASE_PATH + "/test/test_files/"
         ext = "yaml"
-        want_list = ["ecommerce-cards-1.21-en.yaml",
-                     "ecommerce-mappings-1.2.yaml"]
+        want_files = ["ecommerce-cards-1.21-en.yaml",
+                      "ecommerce-mappings-1.2.yaml"]
+        want_count = len(want_files)
 
-        got_list = c.get_files_from_of_type(c, path, ext)
-        got_files = list(os.path.basename(f) for f in got_list)
-        for f in want_list:
-            self.assertIn(f, got_files)
+        got_files = Convert.get_files_from_of_type(Convert, path, ext)
+        self.assertEqual(len(got_files), want_count)
+        got_files = list(os.path.basename(f) for f in got_files)
+        self.assertListEqual(got_files, want_files)
+        # for f in want_files:
+        #     self.assertIn(f, got_files)
 
     def test_get_files_from_of_type_source_docx_files(self) -> None:
-        c.args = argparse.Namespace(debug=False)
-        path = c.BASE_PATH + "/test/test_files/"
+        Convert.args = argparse.Namespace(debug=False)
+        path = Convert.BASE_PATH + "/test/test_files/"
         ext = "docx"
-        want_list = ["owasp_cornucopia_edition_lang_ver_template.docx"]
+        want_files = ["owasp_cornucopia_edition_lang_ver_template.docx"]
 
-        got_list = c.get_files_from_of_type(c, path, ext)
-        got_files = list(os.path.basename(f) for f in got_list)
-        for f in want_list:
+        got_files = Convert.get_files_from_of_type(Convert, path, ext)
+        got_files = list(os.path.basename(f) for f in got_files)
+        self.assertListEqual(got_files, want_files)
+        for f in want_files:
             self.assertIn(f, got_files)
 
     def test_get_files_from_of_type_source_empty_list(self) -> None:
-        c.args = argparse.Namespace(debug=False)
-        path = c.BASE_PATH + "/test/test_files/"
+        Convert.args = argparse.Namespace(debug=False)
+        path = Convert.BASE_PATH + "/test/test_files/"
         ext = "ext"
-        want_list = []
+        want_files = []
 
-        got_list = c.get_files_from_of_type(c, path, ext)
-        self.assertListEqual(got_list, want_list)
+        got_files = Convert.get_files_from_of_type(Convert, path, ext)
+        self.assertListEqual(got_files, want_files)
 
 
 class TestGetDocxDocument(unittest.TestCase):
     def test_get_docx_document_success(self) -> None:
-        c.args = argparse.Namespace(debug=False)
-        file = c.BASE_PATH + "/test/test_files/owasp_cornucopia_edition_lang_ver_template.docx"
+        Convert.args = argparse.Namespace(debug=False)
+        file = Convert.BASE_PATH + "/test/test_files/owasp_cornucopia_edition_lang_ver_template.docx"
         want_type = docx.document.Document
         want_len_paragraphs = 36
 
+        got_file = Convert.get_docx_document(file)
+        got_type = type(got_file)
+        self.assertEqual(want_type, got_type)
+        got_len_paragraphs = len(got_file.paragraphs)
+        self.assertEqual(want_len_paragraphs, got_len_paragraphs)
 
-# print("--- doc first 500:\n" + str(type(doc_test)))
-# print("--- doc first 500:\n" + str(isinstance(doc_test, docx.document.Document)))
-# print("--- doc first 500:\n" + str(len(doc_test.paragraphs)));
-# exit()
+    def test_get_docx_document_failure(self) -> None:
+        Convert.args = argparse.Namespace(debug=False)
+        logging.basicConfig(level=logging.CRITICAL)
+        file = Convert.BASE_PATH + "/test/test_files/owasp_cornucopia_edition_lang_ver_template.d"
+        want_type = docx.document.Document
+        want_len_paragraphs = 0
 
+        got_file = Convert.get_docx_document(file)
+        self.assertIsInstance(got_file, want_type)
+        got_len_paragraphs = len(got_file.paragraphs)
+        self.assertEqual(want_len_paragraphs, got_len_paragraphs)
+
+
+class TestGetTagForSuitName(unittest.TestCase):
+    def test_get_tag_for_suit_name_ve(self) -> None:
+        Convert.args = argparse.Namespace(debug=False)
+        suit = {"name": "Data validation & encoding",
+                "cards": []}
+        suit_tag = "VE"
+        want_tag_data = {"${VE_suit}": suit["name"]}
+
+        got_tag_data = Convert.get_tag_for_suit_name(Convert, suit, suit_tag)
+        self.assertDictEqual(want_tag_data, got_tag_data)
+
+
+class TestGetReplacementDict(unittest.TestCase):
+    test_data = {'meta': {"edition": "ecommerce",
+                          "component": "cards",
+                          "language": "EN",
+                          "version": "1.21"
+                          },
+                 'suits': [{"name": "Data validation & encoding",
+                            "cards": [{"value": "A",
+                                       "desc": "You have invented a new attack against Data Validation",
+                                       "misc": "Read more about this topic in OWASP's free Cheat Sheets"},
+                                      {"value": "2",
+                                       "desc": "Brian can gather information about the underlying configurations"},
+                                      ]
+                            },
+                           {"name": "Authentication",
+                            "cards": [{"value": "A",
+                                       "desc": "You have invented a new attack against Authentication",
+                                       "misc": "Read more about this topic in OWASP's free Cheat Sheet"},
+                                      {"value": "2",
+                                       "desc": "James can undertake authentication functions without"},
+                                      ]
+                            }
+                           ]
+                 }
+
+    def test_get_replacement_dict_success(self) -> None:
+        Convert.args = argparse.Namespace(debug=False)
+        logging.basicConfig(level=logging.ERROR)
+        input_data = self.test_data
+        want_type = dict
+        want_length = 8
+        want_data = {"${VE_suit}": "Data validation & encoding",
+                     "${VE_VEA_desc}": "You have invented a new attack against Data Validation",
+                     "${VE_VEA_misc}": "Read more about this topic in OWASP's free Cheat Sheets",
+                     "${VE_VE2_desc}": "Brian can gather information about the underlying configurations",
+                     "${AT_suit}": "Authentication",
+                     "${AT_ATA_desc}": "You have invented a new attack against Authentication",
+                     "${AT_ATA_misc}": "Read more about this topic in OWASP's free Cheat Sheet",
+                     "${AT_AT2_desc}": "James can undertake authentication functions without",
+                     }
+
+        got_data = Convert.get_replacement_dict(Convert, input_data)
+        self.assertIsInstance(got_data, want_type)
+        self.assertEqual(len(got_data), want_length)
+        self.assertDictEqual(got_data, want_data)
