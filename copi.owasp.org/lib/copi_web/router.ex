905284e50db523a4cc1c41369eaddbe4a310f434
Toby Irvine <toby@securedelivery.io>
2021-08-09 18:56:32 +0100

    reverting content security policy change as it breaks drag and drop

--- copi.owasp.org/lib/copi_web/router.ex
+++ copi.owasp.org/lib/copi_web/router.ex
@@ -7,7 +7,7 @@ defmodule CopiWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, {CopiWeb.LayoutView, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
+    plug :put_secure_browser_headers
   end
 
   pipeline :api do
