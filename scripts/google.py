c34e3d58dcc09291915d6e0a97f4f448ac5f3ca3
Artim-Banyte <artimbanyte@gmail.com>
2021-08-23 09:14:32 +0100

    updated google.py frive

--- scripts/google.py
+++ scripts/google.py
@@ -1,73 +1,113 @@
-from __future__ import print_function
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import json
+import requests
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+gauth = GoogleAuth()
 
-def main():
-    state = flask.session['state']
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        'client_secret.json',
-        scopes=['https://www.googleapis.com/auth/drive.metadata.readonly'],
-        state=state)
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
-    authorization_response = flask.request.url
-    flow.fetch_token(authorization_response=authorization_response)
+def upload():
+    headers = {"Authorization": "Bearer ya29.a0ARrdaM-fW6IdnWdu_mt-EXf_lxRU7vJn2Q3Weaf2CpvblUlilX-s9tTJgNo7AjsaOpzoDI-SbGMN96Pbb18Y0x7Td2-AkOUshCCGsh13MSmTU37q_lXLacOhzhkMxHtZXZBXFQvq38XnzMVUruWSfcF-heDj"}
+    para = {
+        "name": "testfile.png",
+    }
+    files = {
+        'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
+        'file': open("./testimage.png", "rb")
+    }
+    r = requests.post(
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+        headers=headers,
+        files=files
+    )
+    print(r.text)
 
-    # Store the credentials in the session.
-    # ACTION ITEM for developers:
-    #     Store user's access and refresh tokens in your data store if
-    #     incorporating this code into your real app.
-    credentials = flow.credentials
-    flask.session['credentials'] = {
-        'token': credentials.token,
-        'refresh_token': credentials.refresh_token,
-        'token_uri': credentials.token_uri,
-        'client_id': credentials.client_id,
-        'client_secret': credentials.client_secret,
-        'scopes': credentials.scopes}
+upload()
 
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
 
-    service = build('drive', 'v3', credentials=creds)
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
 
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
 
-if __name__ == '__main__':
-    main()
-# [END drive_quickstart]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from __future__ import print_function
+# import os.path
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+#
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+#
+# def main():
+#     """Shows basic usage of the Drive v3 API.
+#     Prints the names and ids of the first 10 files the user has access to.
+#     """
+#     creds = None
+#     # The file token.json stores the user's access and refresh tokens, and is
+#     # created automatically when the authorization flow completes for the first
+#     # time.
+#     if os.path.exists('token.json'):
+#         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+#     # If there are no (valid) credentials available, let the user log in.
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 'credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         # Save the credentials for the next run
+#         with open('token.json', 'w') as token:
+#             token.write(creds.to_json())
+#
+#     service = build('drive', 'v3', credentials=creds)
+#
+#     # Call the Drive v3 API
+#     results = service.files().list(
+#         pageSize=10, fields="nextPageToken, files(id, name)").execute()
+#     items = results.get('files', [])
+#
+#     if not items:
+#         print('No files found.')
+#     else:
+#         print('Files:')
+#         for item in items:
+#             print(u'{0} ({1})'.format(item['name'], item['id']))
+#
+# if __name__ == '__main__':
+#     main()
+# # [END drive_quickstart]
+#
