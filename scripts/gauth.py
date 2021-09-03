a4f1bdc17af5435eccd83d2c92650814f6eff622
GDB @ dell7490 Win <gdb@roamingsolutions.net>
2021-09-03 18:23:42 +0200

    working version with partial gdrive code

--- scripts/gauth.py
+++ scripts/gauth.py
@@ -1,46 +1,42 @@
 from __future__ import print_function
-import httplib2
 import os
 
-from apiclient import discovery
-from oauth2client import client
+from oauth2client import client  # type: ignore
 from oauth2client import tools
-from oauth2client.file import Storage
+from oauth2client.file import Storage  # type: ignore
+import argparse
+import typing
 
 try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    flags: typing.Union[None, argparse.Namespace] = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
 
-class auth:
-    def __init__(self,SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME):
-        self.SCOPES = SCOPES
-        self.CLIENT_SECRET_FILE = CLIENT_SECRET_FILE
-        self.APPLICATION_NAME = APPLICATION_NAME
-    def getCredentials(self):
+class Auth:
+    def __init__(self, scopes: str, client_secret_file: str, application_name: str) -> None:
+        self.SCOPES = scopes
+        self.CLIENT_SECRET_FILE = client_secret_file
+        self.APPLICATION_NAME = application_name
+
+    def get_credentials(self) -> tools.client:
         """Gets valid user credentials from storage.
         If nothing has been stored, or if the stored credentials are invalid,
         the OAuth2 flow is completed to obtain the new credentials.
         Returns:
             Credentials, the obtained credential.
         """
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
+        home_dir = os.path.expanduser("~")
+        credential_dir = os.path.join(home_dir, ".credentials")
         if not os.path.exists(credential_dir):
             os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,
-                                       'drive-python-quickstart.json')
+        credential_path = os.path.join(credential_dir, "drive-python-quickstart.json")
 
         store = Storage(credential_path)
         credentials = store.get()
         if not credentials or credentials.invalid:
             flow = client.flow_from_clientsecrets(self.CLIENT_SECRET_FILE, self.SCOPES)
             flow.user_agent = self.APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else: # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
+            credentials = tools.run_flow(flow, store, flags)
+            print("Storing credentials to " + credential_path)
         return credentials
