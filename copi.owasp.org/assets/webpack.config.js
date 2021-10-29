99f4724b253cf535df6498ab751de7d1487c95d2
Toby Irvine <toby@securedelivery.io>
2021-10-29 12:31:03 +0100

    making EoP cards look like EoP cards

--- copi.owasp.org/assets/webpack.config.js
+++ copi.owasp.org/assets/webpack.config.js
@@ -40,6 +40,14 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
         }
       ]
     },
