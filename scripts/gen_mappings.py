f945aa32ae319ec8b72d1aa55a78420edf45c60b
Grant Ongers <grant@ongers.net>
2023-07-07 23:49:53 +0100

    Mapping link generic.

--- scripts/gen_mappings.py
+++ scripts/gen_mappings.py
@@ -1,8 +1,6 @@
 import argparse
 import yaml
 import requests
-import qrcode  # type: ignore
-import qrcode.image.svg  # type: ignore
 from typing import Any, Dict
 
 opencre_base_url = "https://opencre.org"
@@ -15,71 +13,65 @@ STANDARDS_TO_ADD = [
 ]
 
 
-def make_cre_link(cre_id: str, frontend: bool = False) -> str:
+def make_mapping_link(mapping_id: str, mapping_type: str, frontend: bool = False) -> str:
     if frontend:
-        return f"{opencre_base_url}/cre/{cre_id}"
+        return f"{opencre_base_url}/cre/{mapping_id}"
     else:
-        return f"{opencre_rest_url}/id/{cre_id}"
+        return f"{opencre_rest_url}/id/{mapping_id}"
 
 
-def produce_ecommerce_mappings(source_file: Dict[Any, Any], standards_to_add: list[str]) -> Dict[Any, Any]:
+def produce_ecommerce_mappings(
+    source_file: Dict[Any, Any], standards_to_add: list[str], mapping_type: str
+) -> Dict[Any, Any]:
     base = {
         "meta": {"edition": "ecommerce", "component": "mappings", "language": "ALL", "version": CORNUCOPIA_VERSION},
     }
     for indx, suit in enumerate(source_file.copy()["suits"]):
         for card_indx, card in enumerate(suit["cards"]):
-            cre = card["cre"][0]
-            response = requests.get(make_cre_link(cre))
-            if response.status_code == 200:
-                cre_object = response.json().get("data")
-                for standard in standards_to_add:
-                    for link in cre_object.get("links"):
-                        if link.get("document").get("name") == standard:
-                            source_file["suits"][indx]["cards"][card_indx][standard] = link.get("document").get(
-                                "sectionID"
-                            )
-            else:
-                print(f"could not find CRE {cre}, status code {response.status_code}")
+            for mapping_id in card[mapping_type]:
+                response = requests.get(make_mapping_link(mapping_id, mapping_type))
+                if response.status_code == 200:
+                    cre_object = response.json().get("data")
+                    for standard in standards_to_add:
+                        for link in cre_object.get("links"):
+                            if link.get("document").get("name") == standard:
+                                source_file["suits"][indx]["cards"][card_indx][standard] = link.get("document").get(
+                                    "sectionID"
+                                )
+                else:
+                    print(f"could not find {mapping_type} {mapping_id}, status code {response.status_code}")
 
     base["suits"] = source_file["suits"]
     return base
 
 
-def generate_qr_images(existing_mappings: Dict[Any, Any], directory_path: str) -> None:
-    for suit in existing_mappings["suits"]:
-        for card in suit["cards"]:
-            cre = card["cre"][0]
-            link = make_cre_link(cre, frontend=True)
-            print(f"making qr code for {cre}")
-            img = qrcode.make(link, image_factory=qrcode.image.svg.SvgImage)
-            with open(f"{directory_path}/{cre}", "wb") as f:
-                img.save(f)
-
-
 def main() -> None:
     global opencre_base_url, opencre_rest_url
     parser = argparse.ArgumentParser(description="generate mappings")
-    parser.add_argument("-c", "--cres", help="Where to find the file mapping cornucopia to CREs", required=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-c", "--cre", help="Where to find the file mapping cornucopia to CREs")
+    group.add_argument("-k", "--capec", help="Where to find the file mapping cornucopia to CAPEC")
     parser.add_argument("-t", "--target", help="Path where to store the result", required=True)
     parser.add_argument(
         "-s", "--staging", action="store_true", help="If provided will use staging.opencre.org instead of opencre.org"
-    )
-    parser.add_argument(
-        "-q", "--qr_images", help="If provided will populate the target dir with qr image pointing to every cre"
     )
     args = vars(parser.parse_args())
     if args["staging"]:
         print("Using staging.opencre.org")
         opencre_base_url = "https://staging.opencre.org"
         opencre_rest_url = "https://staging.opencre.org/rest/v1"
-    with open(args["cres"]) as f:
+    if args["cre"]:
+        basefile = args["cre"]
+        mapping_base = "cre"
+    elif args["capec"]:
+        basefile = args["capec"]
+        mapping_base = "capec"
+    with open(basefile) as f:
         mappings = yaml.safe_load(f)
         if args["target"]:
-            ecommerce = produce_ecommerce_mappings(mappings, STANDARDS_TO_ADD)
+            ecommerce = produce_ecommerce_mappings(mappings, STANDARDS_TO_ADD, mapping_base)
             with open(args["target"], "w") as ef:
                 yaml.safe_dump(ecommerce, ef)
-        if args["qr_images"]:
-            generate_qr_images(mappings, args["qr_images"])
 
 
 if __name__ == "__main__":
