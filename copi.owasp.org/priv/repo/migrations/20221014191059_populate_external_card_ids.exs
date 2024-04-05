6a3df559be7e02489eb5ec8ac280b2e0f1835902
Xavier Godard <xavier.godard@xdesign.com>
2024-04-05 23:25:11 +0100

    updated migrations files so that we can migrate the table even though there is no data in the database

--- copi.owasp.org/priv/repo/migrations/20221014191059_populate_external_card_ids.exs
+++ copi.owasp.org/priv/repo/migrations/20221014191059_populate_external_card_ids.exs
@@ -9,24 +9,53 @@ defmodule Copi.Repo.Migrations.PopulateExternalCardIds do
 
     case YamlElixir.read_from_file(path) do
       {:ok, cards} ->
+        edition = cards["meta"]["edition"]
+        language = cards["meta"]["language"]
+        version = cards["meta"]["version"]
+
         for suit <- cards["suits"] do
           for card <- suit["cards"] do
-             Repo.get_by!(Card, [category: suit["name"], value: card["value"]])
-            |> Ecto.Changeset.change(external_id: card["id"])
-            |> Repo.update()
+            currentCard = Repo.get_by(Card, category: suit["name"], value: card["value"])
+
+            if currentCard do
+              card
+              |> Ecto.Changeset.change(external_id: card["id"])
+              |> Repo.update()
+            else
+              misc = if Map.has_key?(card, "misc"), do: card["misc"], else: ""
+
+              Repo.insert!(%Card{
+                edition: edition,
+                language: language,
+                version: version,
+                category: suit["name"],
+                value: card["value"],
+                description: card["desc"],
+                misc: misc
+              })
+            end
           end
         end
     end
 
-    path = Path.join(File.cwd!(), "priv/repo/cornucopia/eop-cards--1.0-en.yaml")
+    path = Path.join(File.cwd!(), "priv/repo/cornucopia/ecommerce-mappings-1.2.yaml")
 
     case YamlElixir.read_from_file(path) do
       {:ok, cards} ->
         for suit <- cards["suits"] do
           for card <- suit["cards"] do
-             Repo.get_by!(Card, [category: suit["name"], value: card["value"]])
-            |> Ecto.Changeset.change(external_id: card["id"])
-            |> Repo.update()
+            this_card = Repo.get_by!(Card, category: suit["name"], value: card["value"])
+
+            this_card =
+              Ecto.Changeset.change(this_card,
+                owasp_scp: Enum.map(card["owasp_scp"], fn x -> to_string(x) end),
+                owasp_asvs: Enum.map(card["owasp_asvs"], fn x -> to_string(x) end),
+                owasp_appsensor: Enum.map(card["owasp_appsensor"], fn x -> to_string(x) end),
+                capec: Enum.map(card["capec"], fn x -> to_string(x) end),
+                safecode: Enum.map(card["safecode"], fn x -> to_string(x) end)
+              )
+
+            Repo.update(this_card)
           end
         end
     end
--- copi.owasp.org/priv/repo/migrations/20221014193927_index_external_card_ids.exs
+++
@@ -1,7 +0,0 @@
-defmodule Copi.Repo.Migrations.IndexExternalCardIds do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:cards, [:external_id])
-  end
-end
