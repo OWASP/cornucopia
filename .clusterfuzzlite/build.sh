2d1bb37d46cdc70ba5039206bde1f5d6a7374cb3
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-04-07 15:07:32 +0200

    Adding hypothesis test example and start on the first cornucopia fuzzer

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,10 +1,36 @@
 #!/bin/bash -eu
+# How to test locally:
+# cd ../
+# git clone https://github.com/google/oss-fuzz
+# cd ../oss-fuzz
+# export PATH_TO_PROJECT=/home/sydseter/shared/cornucopia
+# python infra/helper.py build_fuzzers --external $PATH_TO_PROJECT --sanitizer address
+# python infra/helper.py build_image --external $PATH_TO_PROJECT
+# python infra/helper.py check_build --external $PATH_TO_PROJECT --sanitizer address
 
 # build project
-# e.g.
-# ./autogen.sh
-# ./configure
-# make -j$(nproc) all
+update-alternatives --set python3 /usr/bin/python3.10
+#pip install nuitka
+python3 -m pip install -r requirements.txt --require-hashes
+python3 -m pip  install -r install_cornucopia_deps.txt --require-hashes --no-deps
+
+#for debugging
+#exec "$SHELL"
+# Build fuzzers into $OUT. These could be detected in other ways.
+for fuzzer in $(find "$SRC/tests/scripts" -name '*_fuzzer.py'); do
+  fuzzer_basename=$(basename -s .py $fuzzer)
+  fuzzer_package=${fuzzer_basename}.pkg
+
+  #python3 -m nuitka3 --output-dir=$OUT --onefile --output-filename=$fuzzer_package $fuzzer
+  python3 -m PyInstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
+
+  echo "#!/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
+this_dir=\$(dirname \"\$0\")
+ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
+\$this_dir/tests/scripts/$fuzzer_package \$@" > $OUT/$fuzzer_basename
+  chmod +x $OUT/$fuzzer_basename
+done
 
 # build fuzzers
 # e.g.
