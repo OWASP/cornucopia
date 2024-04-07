fd56fec5eba5fd1427f34f8414980bf9c3acc185
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-04-07 20:52:58 +0200

    check out where the fuzzing takes place.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -37,7 +37,7 @@ this_dir=\$(dirname \"\$0\")
 echo "this_dir=$this_dir"
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
 \$this_dir/tests/scripts/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
+  chmod +x "$OUT/$fuzzer_basename.pkg"
 done
 
 # build fuzzers
