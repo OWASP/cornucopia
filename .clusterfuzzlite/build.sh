b015068736c3f8652b6d52c839dbb2c931a3e07c
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-04-07 21:50:57 +0200

    fix unbound variable error.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -33,9 +33,10 @@ for fuzzer in $(find "$SRC/cornucopia/tests/scripts" -name '*_fuzzer.py'); do
 echo "fuzzing now, this is what is here"
 this_dir=\$(dirname \"\$0\")
 echo "this_dir=\$this_dir"
+pwd
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
 \$this_dir/tests/scripts/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x "$OUT/$fuzzer_basename.pkg"
+  chmod +x "$OUT/$fuzzer_basename"
 done
 
 # build fuzzers
