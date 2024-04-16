24ebe9de427f471d459517cfc73119c2a2254dc4
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-04-16 13:15:11 +0200

    build environment using python3.10 and ensure all dependencies are pinned.

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,32 +1,15 @@
 #!/bin/bash -eu
-# How to test locally from inside the cornucopia project:
-# export PATH_TO_PROJECT=$(pwd)
-# cd ../
-# git clone https://github.com/google/oss-fuzz
-# cd ../oss-fuzz
-# python infra/helper.py build_fuzzers --external $PATH_TO_PROJECT --sanitizer address
-# python infra/helper.py build_image --external $PATH_TO_PROJECT
-# python infra/helper.py check_build --external $PATH_TO_PROJECT --sanitizer address
 
 # build project
-update-alternatives --set python3 /usr/bin/python3.10
-#pip install nuitka
-python3 -m pip install -r requirements.txt --require-hashes
-python3 -m pip  install -r install_cornucopia_deps.txt --require-hashes --no-deps
+python3.10 -m pip install -r requirements.txt --require-hashes
+python3.10 -m pip install -r install_cornucopia_deps.txt
 
-echo "What is here?"
-ls
-pwd
-echo "OUT_DIR=$OUT"
-#for debugging
-#exec "$SHELL"
 # Build fuzzers into $OUT. These could be detected in other ways.
 for fuzzer in $(find "$SRC/cornucopia/tests/scripts" -name '*_fuzzer.py'); do
   fuzzer_basename=$(basename -s .py $fuzzer)
   fuzzer_package=${fuzzer_basename}.pkg
 
-  #python3 -m nuitka3 --output-dir=$OUT --onefile --output-filename=$fuzzer_package $fuzzer
-  python3 -m PyInstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
+  python3.10 -m PyInstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer --exclude IPython
 
   echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
@@ -36,9 +19,3 @@ ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm
 \$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod +x "$OUT/$fuzzer_basename"
 done
-
-# build fuzzers
-# e.g.
-# $CXX $CXXFLAGS -std=c++11 -Iinclude \
-#     /path/to/name_of_fuzzer.cc -o $OUT/name_of_fuzzer \
-#     $LIB_FUZZING_ENGINE /path/to/library.a
