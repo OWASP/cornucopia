6615089c5ccfc422f886f89676f7cf8027547f36
Johan Sydseter <johan.sydseter@admincontrol.com>
2024-04-25 00:36:01 +0200

    Update tests.

--- tests/scripts/gen_mappings_utest.py
+++ tests/scripts/gen_mappings_utest.py
@@ -12,12 +12,20 @@ if "unittest.util" in __import__("sys").modules:
 class TestProduceEcommerceMappings(unittest.TestCase):
     def test_can_produce_ecommerce_mappings(self):
         test_input = {
+            "meta": {
+                "language": "EN",
+                "version": "1.30"
+            },
             "suits": [
                 {"cards": [{"cre": ["308-515"], "value": "2"}], "name": "Data validation & encoding", "code": "VE"},
                 {"cards": [{"cre": ["138-448"], "value": "2"}], "name": "Session management", "code": "SM"},
             ]
         }
         language_mapping = {
+            "meta": {
+                "language": "EN",
+                "version": "1.30"
+            },
             "suits": [
                 {"name": "Data validation & encoding", "cards": [{"desc": "DV2 test"}]},
                 {"name": "Session management", "cards": [{"desc": "SM2 test"}]},
@@ -70,6 +78,10 @@ class MainTestCase(unittest.TestCase):
                 with open("output/mapping.json", "w") as mock_write:
                     mock_write("test")
                     mock_load.return_value = {
+                        "meta": {
+                            "language": "EN",
+                            "version": "1.30"
+                        },
                         "suits": [
                             {
                                 "cards": [{"cre": ["308-515"], "value": "2", "desc": ""}],
