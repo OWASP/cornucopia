ca4295054b672d61527fbe5549bb90485bcd8dfd
Jef Meijvis <jef.meijvis@hotmail.com>
2024-11-20 22:43:17 +0100

    Sign: Added card colors for mobile card deck

--- src/domain/card/cardColor.ts
+++ src/domain/card/cardColor.ts
@@ -1,11 +1,17 @@
 export const cardColor = new Map<string,string>([
     // Color mapping for the card rendering. 
-    // Mobile edition suits can also be added here
+    // Webapp suits
     ["authentication", "#8FC4E8"],
     ["authorization", "#ECCE3E"],
     ["data-validation-&-encoding", "#B5B1AF"],
     ["cornucopia", "#526994"],
     ["session-management", "#B1D579"],
     ["cryptography", "#C3B2E3"],
-    ["wild-card", "#FBB67C"]
+    ["wild-card", "#FBB67C"],
+    // Mobileapp cards
+    ["platform-&-code", "#00FFBF"],
+    ["authentication-&-authorization", "#FFD500"],
+    ["network-&-storage", "#FF6600"],
+    ["resilience", "#006EFF"],
+    ["cryptography", "#006EFF"]
 ]) 
