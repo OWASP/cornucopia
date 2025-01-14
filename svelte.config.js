5f0d400c9e94dfcf3cc89eb4ea0bf2a3be97f329
Johan Sydseter <johan.sydseter@admincontrol.com>
2025-01-14 12:02:46 +0100

    Add cso for vite preview

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,37 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { VERCEL_ENV } from '$env/static/private';
+
+let csp = {
+	mode: "auto",
+	directives: {
+		'default-src': ['none'],
+		'connect-src': ['self'],
+		'script-src': ['self'],
+		'script-src-elem': ['self'],
+		'style-src': ['self'],
+		'style-src-elem': ['self'],
+		'img-src': ['self', 'https://i.ytimg.com/vi/XXTPXozIHow/mqdefault.jpg'],
+		'frame-src': ['self', 'https://www.youtube.com/']
+	}
+};
+
+if (VERCEL_ENV == 'preview') {
+	csp = {
+		mode: "auto",
+		directives: {
+			'default-src': ['none'],
+			'connect-src': ['self', 'https://vercel.live/', 'https://vercel.com', '*.pusher.com', '*.pusherapp.com'],
+			'script-src': ['self', 'https://vercel.live/', 'https://vercel.com', 'unsafe-inline'],
+			'script-src-elem': ['self', 'https://vercel.live/', 'https://vercel.com'],
+			'style-src': ['self'],
+			'style-src-elem': ['self'],
+			'img-src': ['self', 'https://i.ytimg.com/vi/XXTPXozIHow/mqdefault.jpg', 'https://vercel.live/', 'https://vercel.com', '*.pusher.com/', 'data: blob'],
+			'frame-src': ['self', 'https://www.youtube.com/', 'https://vercel.live/', 'https://vercel.com']
+		}
+	};
+}
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = 
@@ -20,19 +52,7 @@ const config =
 				throw new Error(message);
 			}
 		},
-		csp: {
-			mode: "auto",
-			directives: {
-				'default-src': ['none'],
-				'connect-src': ['self'],
-				'script-src': ['self'],
-				'script-src-elem': ['self', 'https://vercel.live/'],
-				'style-src': ['self'],
-				'style-src-elem': ['self'],
-				'img-src': ['self', 'https://i.ytimg.com/vi/XXTPXozIHow/mqdefault.jpg'],
-				'frame-src': ['self', 'https://www.youtube.com/']
-			}
-		},
+		csp: csp,
 		csrf: {
 			checkOrigin: true
 		}
