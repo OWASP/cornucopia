d325630ee42c9ffb4f761ae50654a003b4b45459
Johan Sydseter <johan.sydseter@admincontrol.com>
2025-01-24 17:28:55 +0100

    add base-uri

--- cornucopia.owasp.org/script/headers.js
+++ cornucopia.owasp.org/script/headers.js
@@ -27,11 +27,11 @@ function main() {
   Referrer-Policy: same-origin
   Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(self "https://www.youtube.com/"), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()
   Strict-Transport-Security: max-age=31536000; includeSubDomains; preload
-  content-security-policy: default-src 'none'; connect-src 'self'; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; script-src-elem 'self' 'nonce-DhcnhD3khTMePgXw'; style-src 'self'; style-src-elem 'self'; img-src 'self'
+  content-security-policy: default-src 'none'; base-uri: 'self'; connect-src 'self'; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; script-src-elem 'nonce-DhcnhD3khTMePgXw'; style-src 'self'; style-src-elem 'self'; img-src 'self'
 
 /how-to-play
   Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(self "https://www.youtube.com/"), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()
-  content-security-policy: default-src 'none'; frame-src 'self' https://www.youtube.com/; connect-src 'self'; img-src 'self' https://i.ytimg.com/vi/XXTPXozIHow/mqdefault.jpg; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; script-src-elem 'self' 'nonce-DhcnhD3khTMePgXw'; style-src 'self'; style-src-elem 'self'
+  content-security-policy: default-src 'none'; base-uri: 'self'; frame-src 'self' https://www.youtube.com/; connect-src 'self'; img-src 'self' https://i.ytimg.com/vi/XXTPXozIHow/mqdefault.jpg; script-src 'self' 'nonce-DhcnhD3khTMePgXw'; script-src-elem 'nonce-DhcnhD3khTMePgXw'; style-src 'self'; style-src-elem 'self'
 `;
 
   const headersFile = path.join(buildDir, '_headers');
