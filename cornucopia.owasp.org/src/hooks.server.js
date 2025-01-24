067c889cf054167f81737391e10e02c4b88de479
Johan Sydseter <johan.sydseter@admincontrol.com>
2025-01-24 17:12:31 +0100

    Adding static nonce

--- cornucopia.owasp.org/src/hooks.server.js
+++ cornucopia.owasp.org/src/hooks.server.js
@@ -19,7 +19,7 @@ export const handle = async ({ event, resolve }) => {
 
   // Add html `lang` attribute
   const response = await resolve({ ...event, locals: { lang: userLocale, translation: translations.get()[userLocale], fallbackTranslation: translations.get()[defaultLocale] } }, {
-    transformPageChunk: ({ html }) => html.replace(/<html.*>/, `<html lang="${userLocale}">`),
+    transformPageChunk: ({ html }) => html.replace(/<html.*>/, `<html lang="${userLocale}">`).replaceAll(/<script.*>/, `<script nonce="DhcnhD3khTMePgXw">`),
   });
 
   const securityHeaders = {
