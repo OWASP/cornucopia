9cbc32eea63897b6a16d15afdd3319e1c423f21f
Johan Sydseter <johan.sydseter@admincontrol.com>
2025-02-10 18:45:00 +0100

    use the cloudflare adapter to ensure the 404 falback is applied

--- cornucopia.owasp.org/svelte.config.js
+++ cornucopia.owasp.org/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { config } from 'dotenv';
 config();
