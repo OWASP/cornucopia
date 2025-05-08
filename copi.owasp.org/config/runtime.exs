675f12420446c84b4d0599054f3d7e0083277a16
sydseter <johan.sydseter@admincontrol.com>
2025-05-08 10:51:22 +0200

    add normal ipv4

--- copi.owasp.org/config/runtime.exs
+++ copi.owasp.org/config/runtime.exs
@@ -28,13 +28,14 @@ if config_env() == :prod do
       For example: ecto://postgres:y9EAY7xeVucjM2yM@localhost/copi_dev
       """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+  #maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :copi, Copi.Repo,
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    #socket_options: maybe_ipv6
+    socket_options: [:inet, :inet6]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
