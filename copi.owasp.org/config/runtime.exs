a88ee77dbe10743c96773641ce4572aed37e15e9
sydseter <johan.sydseter@admincontrol.com>
2025-05-18 20:01:26 +0200

    Revert the config

--- copi.owasp.org/config/runtime.exs
+++ copi.owasp.org/config/runtime.exs
@@ -25,12 +25,13 @@ if config_env() == :prod do
     System.get_env("DATABASE_URL") ||
       raise """
       environment variable DATABASE_URL is missing.
-      For example: ecto://postgres:y9EAY7xeVucjM2yM@localhost/copi_dev
+      For example: ecto://USER:PASS@HOST/DATABASE
       """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: [:inet, :inet6]
 
   config :copi, Copi.Repo,
+    # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
