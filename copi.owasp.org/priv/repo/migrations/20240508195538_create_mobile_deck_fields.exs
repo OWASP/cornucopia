8db86f0fa80b369949de29fe795d94664d507cc7
sydseter <johan.sydseter@admincontrol.com>
2025-06-11 18:37:38 +0200

    Removing the remove clause

--- copi.owasp.org/priv/repo/migrations/20240508195538_create_mobile_deck_fields.exs
+++ copi.owasp.org/priv/repo/migrations/20240508195538_create_mobile_deck_fields.exs
@@ -3,8 +3,8 @@ defmodule Copi.Repo.Migrations.CreateMobileDeckFields do
 
   def change do
     alter table(:cards) do
-      Ecto.Migration.remove_if_exists :owasp_masvs, {:array, :string}
-      Ecto.Migration.remove_if_exists :owasp_mastg, {:array, :string}
+      #Ecto.Migration.remove_if_exists :owasp_masvs, {:array, :string}
+      #Ecto.Migration.remove_if_exists :owasp_mastg, {:array, :string}
       add :owasp_masvs, {:array, :string}
       add :owasp_mastg, {:array, :string}
     end
