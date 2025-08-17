811af0526d90b6a5dcd27eb12df6bfa03f168421
sydseter <johan.sydseter@admincontrol.com>
2025-08-17 23:21:15 +0200

    Fix script and generate the controls

--- cornucopia.owasp.org/src/scripts/createASVS.py
+++ cornucopia.owasp.org/src/scripts/createASVS.py
@@ -1,5 +1,5 @@
 # This script converts the JSON from
-# //github.com/OWASP/ASVS/blob/master/4.0/docs_en/OWASP%20Application%20Security%20Verification%20Standard%204.0.3-en.json
+# OWASP_Application_Security_Verification_Standard_5.0.0_en.json
 # to the file structure that our cornucopia website can understand.
 import shutil
 import json
@@ -12,7 +12,7 @@ def createLevelSummary(level, arr):
     topic = ""
     category = ""
     os.mkdir(mypath + f"/level-{level}-controls")
-    f = open(mypath + f"/level-{level}-controls/index.md", "w")
+    f = open(mypath + f"/level-{level}-controls/index.md", "w", encoding="utf-8")
     f.write(f"# Level {level} controls\r\n")
     f.write(f"Level {level} contains {len(arr)} controls listed below: \r\n")
     for link in arr:
@@ -52,7 +52,6 @@ def main():
             itemname = (
                 str(item["Ordinal"]).rjust(2, "0") + "-" + item["Name"].lower().replace(" ", "-").replace(",", "")
             )
-            print(itemname)
             print(item)
             thispath = mypath + "/" + name + "/" + itemname
             os.mkdir(thispath)
@@ -79,18 +78,21 @@ def main():
                     "link": link,
                     "description": description,
                 }
-                #if subitem["L1"]["Required"]:
-                #    L1.append(obj)
+                if int(subitem["L"]) == 1:
+                    L1.append(obj)
 
-                #if subitem["L2"]["Required"]:
-                #    L2.append(obj)
+                if int(subitem["L"]) == 2:
+                    L2.append(obj)
 
-                #if subitem["L3"]["Required"]:
-                #    L3.append(obj)
+                if int(subitem["L"]) == 3:
+                    L3.append(obj)
 
-                #cwe = str(subitem["CWE"]).replace("[", "").replace("]", "")
-                #f.write("CWE: [" + cwe + "](https://cwe.mitre.org/data/definitions/" + cwe + ")\r\n")
+                # cwe = str(subitem["CWE"]).replace("[", "").replace("]", "")
+                # f.write("CWE: [" + cwe + "](https://cwe.mitre.org/data/definitions/" + cwe + ")\r\n")
+                print("object:")
+                print(obj)
                 print("🟪")
+
                 print(subitem)
             f.write("\r\n")
             f.write("## Disclaimer:\r\n")
@@ -105,9 +107,9 @@ def main():
             f.write("\r\n")
             f.close()
 
-    #createLevelSummary(1, L1)
-    #createLevelSummary(2, L2)
-    #createLevelSummary(3, L3)
+    createLevelSummary(1, L1)
+    createLevelSummary(2, L2)
+    createLevelSummary(3, L3)
 
     print("DONE")
 
