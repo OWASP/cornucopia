cdb341929f25291f3f907750bb582895655d4a98
sydseter <johan.sydseter@admincontrol.com>
2025-10-07 01:01:15 +0200

    #1431 Add button to be able to more easily copy the url

--- copi.owasp.org/lib/copi_web/components/core_components/buttons.ex
+++ copi.owasp.org/lib/copi_web/components/core_components/buttons.ex
@@ -57,4 +57,25 @@ defmodule CopiWeb.CoreComponents.Buttons do
     </button>
     """
   end
+
+  def copy_url_button(assigns) do
+    ~H"""
+    <div id="copy-url-container" phx-hook="CopyUrl">
+      <button
+        id="copy-url-btn"
+        class="mt-2 rounded-md px-4 py-4 text-sm font-semibold shadow-sm transition-colors bg-gray-100 text-gray-800 hover:bg-gray-300 active:bg-gray-100 active:opacity-50"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 010 5.656l-3.535 3.535a4 4 0 01-5.656-5.656l1.414-1.414M10.172 13.828a4 4 0 010-5.656l3.535-3.535a4 4 0 015.656 5.656l-1.414 1.414" />
+        </svg>
+      </button>
+      <input id="copied-url" class="mt-2 rounded-md px-3 py-3 text-sm font-semibold shadow-sm text-gray-500" value={@uri} />
+      <span id="url-copied" class="hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+        </svg>
+      </span>
+    </div>
+    """
+  end
 end
