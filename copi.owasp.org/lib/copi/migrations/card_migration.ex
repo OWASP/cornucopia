6d6796083739558d1037d6a9cf2c1afbfc5d22f2
Muhammad Awais Mohsin <miihammadawais456@gmail.com>
2026-02-07 17:12:21 +0500

    fix: resolve migration error (language=ALL) and unused variable

--- copi.owasp.org/lib/copi/migrations/card_migration.ex
+++ copi.owasp.org/lib/copi/migrations/card_migration.ex
@@ -1,6 +1,7 @@
 defmodule Copi.CardMigration do
   alias Copi.Repo
   alias Copi.Cornucopia.Card
+  import Ecto.Query
 
   def add_cards_to_database(cards_file_path, mappings_file_path)do
     populate_cards cards_file_path
@@ -48,68 +49,76 @@ defmodule Copi.CardMigration do
     case YamlElixir.read_from_file(path) do
       {:ok, cards} ->
         edition = cards["meta"]["edition"]
-
+        language = cards["meta"]["language"]
         version = cards["meta"]["version"]
         for suit <- cards["suits"] do
           for card <- suit["cards"] do
             biml = if Map.has_key?(card, "biml"), do: card["biml"], else: ""
             devguide = if Map.has_key?(card, "owasp_dev_guide"), do: card["owasp_dev_guide"], else: []
-            this_card = Repo.get_by!(Card, category: suit["name"], value: card["value"], edition: edition, version: version)
 
-            this_card =  case edition do
-             "ecommerce" -> Ecto.Changeset.change(this_card,
-                  owasp_scp: set_mappings_for_card(card["owasp_scp"]),
-                  owasp_devguide: [],
-                  owasp_asvs: set_mappings_for_card(card["owasp_asvs"]),
-                  owasp_masvs: [],
-                  owasp_mastg: [],
-                  owasp_appsensor: set_mappings_for_card(card["owasp_appsensor"]),
-                  capec: set_mappings_for_card(card["capec"]),
-                  safecode: set_mappings_for_card(card["safecode"])
-              )
-              "webapp" -> Ecto.Changeset.change(this_card,
-                  owasp_scp: set_mappings_for_card(card["owasp_scp"]),
-                  owasp_devguide: set_mappings_for_card(devguide),
-                  owasp_asvs: set_mappings_for_card(card["owasp_asvs"]),
-                  owasp_masvs: [],
-                  owasp_mastg: [],
-                  owasp_appsensor: set_mappings_for_card(card["owasp_appsensor"]),
-                  capec: set_mappings_for_card(card["capec"]),
-                  safecode: set_mappings_for_card(card["safecode"])
-              )
-              "masvs" -> Ecto.Changeset.change(this_card,
-                  owasp_scp: [],
-                  owasp_devguide: [],
-                  owasp_asvs: [],
-                  owasp_masvs: set_mappings_for_card(card["owasp_masvs"]),
-                  owasp_mastg: set_mappings_for_card(card["owasp_mastg"]),
-                  owasp_appsensor: [],
-                  capec: set_mappings_for_card(card["capec"]),
-                  safecode: set_mappings_for_card(card["safecode"])
-              )
-              "mlsec" -> Ecto.Changeset.change(this_card,
-                  biml: biml,
-                  owasp_scp: [],
-                  owasp_devguide: [],
-                  owasp_asvs: [],
-                  owasp_masvs: [],
-                  owasp_mastg: [],
-                  owasp_appsensor: [],
-                  capec: [],
-                  safecode: []
-              )
-              "mobileapp" -> Ecto.Changeset.change(this_card,
-                  owasp_scp: [],
-                  owasp_devguide: [],
-                  owasp_asvs: [],
-                  owasp_masvs: set_mappings_for_card(card["owasp_masvs"]),
-                  owasp_mastg: set_mappings_for_card(card["owasp_mastg"]),
-                  owasp_appsensor: [],
-                  capec: set_mappings_for_card(card["capec"]),
-                  safecode: set_mappings_for_card(card["safecode"])
-              )
+            cards_to_update =
+              if language == "ALL" do
+                 Repo.all(from c in Card, where: c.category == ^suit["name"] and c.value == ^card["value"] and c.edition == ^edition and c.version == ^version)
+              else
+                 [Repo.get_by!(Card, category: suit["name"], value: card["value"], edition: edition, language: language, version: version)]
+              end
+
+            for this_card <- cards_to_update do
+              changeset = case edition do
+               "ecommerce" -> Ecto.Changeset.change(this_card,
+                    owasp_scp: set_mappings_for_card(card["owasp_scp"]),
+                    owasp_devguide: [],
+                    owasp_asvs: set_mappings_for_card(card["owasp_asvs"]),
+                    owasp_masvs: [],
+                    owasp_mastg: [],
+                    owasp_appsensor: set_mappings_for_card(card["owasp_appsensor"]),
+                    capec: set_mappings_for_card(card["capec"]),
+                    safecode: set_mappings_for_card(card["safecode"])
+                )
+                "webapp" -> Ecto.Changeset.change(this_card,
+                    owasp_scp: set_mappings_for_card(card["owasp_scp"]),
+                    owasp_devguide: set_mappings_for_card(devguide),
+                    owasp_asvs: set_mappings_for_card(card["owasp_asvs"]),
+                    owasp_masvs: [],
+                    owasp_mastg: [],
+                    owasp_appsensor: set_mappings_for_card(card["owasp_appsensor"]),
+                    capec: set_mappings_for_card(card["capec"]),
+                    safecode: set_mappings_for_card(card["safecode"])
+                )
+                "masvs" -> Ecto.Changeset.change(this_card,
+                    owasp_scp: [],
+                    owasp_devguide: [],
+                    owasp_asvs: [],
+                    owasp_masvs: set_mappings_for_card(card["owasp_masvs"]),
+                    owasp_mastg: set_mappings_for_card(card["owasp_mastg"]),
+                    owasp_appsensor: [],
+                    capec: set_mappings_for_card(card["capec"]),
+                    safecode: set_mappings_for_card(card["safecode"])
+                )
+                "mlsec" -> Ecto.Changeset.change(this_card,
+                    biml: biml,
+                    owasp_scp: [],
+                    owasp_devguide: [],
+                    owasp_asvs: [],
+                    owasp_masvs: [],
+                    owasp_mastg: [],
+                    owasp_appsensor: [],
+                    capec: [],
+                    safecode: []
+                )
+                "mobileapp" -> Ecto.Changeset.change(this_card,
+                    owasp_scp: [],
+                    owasp_devguide: [],
+                    owasp_asvs: [],
+                    owasp_masvs: set_mappings_for_card(card["owasp_masvs"]),
+                    owasp_mastg: set_mappings_for_card(card["owasp_mastg"]),
+                    owasp_appsensor: [],
+                    capec: set_mappings_for_card(card["capec"]),
+                    safecode: set_mappings_for_card(card["safecode"])
+                )
+              end
+              Repo.update changeset
             end
-            Repo.update this_card
           end
         end
     end
--- copi.owasp.org/mix.exs
+++ copi.owasp.org/mix.exs
@@ -38,7 +38,7 @@ defmodule Copi.MixProject do
       {:phoenix, "~> 1.8.0"},
       {:phoenix_ecto, "~> 4.7.0"},
       {:ecto_sql, "~> 3.13.0"},
-      {:postgrex, ">= 0.19.0 and < 0.22.0"},
+      {:postgrex, ">= 0.19.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.11"},
