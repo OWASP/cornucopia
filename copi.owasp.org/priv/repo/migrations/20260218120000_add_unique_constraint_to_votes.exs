d533510b1550f852c53b38edf2220f6655a191fc
immortal71 <newaashish190@gmail.com>
2026-02-18 19:16:28 -0800

    Simplify migration - just create unique index without DELETE

--- copi.owasp.org/priv/repo/migrations/20260218120000_add_unique_constraint_to_votes.exs
+++ copi.owasp.org/priv/repo/migrations/20260218120000_add_unique_constraint_to_votes.exs
@@ -1,25 +1,8 @@
 defmodule Copi.Repo.Migrations.AddUniqueConstraintToVotes do
   use Ecto.Migration
 
-  def up do
-    # Remove any existing duplicate votes first
-    # This runs only if votes table has  data
-    execute """
-    DELETE FROM votes a 
-    WHERE a.id IN (
-      SELECT a2.id FROM votes a2
-      JOIN votes b ON a2.player_id = b.player_id AND a2.dealt_card_id = b.dealt_card_id
-      WHERE a2.id > b.id
-    )
-    """
-
-    # Create unique index
+  def change do
     create unique_index(:votes, [:player_id, :dealt_card_id], 
-      name: :votes_player_dealt_card_unique_index)
-  end
-
-  def down do
-    drop_if_exists index(:votes, [:player_id, :dealt_card_id],
       name: :votes_player_dealt_card_unique_index)
   end
 end
