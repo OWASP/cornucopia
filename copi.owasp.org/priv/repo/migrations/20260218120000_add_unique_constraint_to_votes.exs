e880c1acbd93af0340a872c8a8c6cc650470f03b
immortal71 <newaashish190@gmail.com>
2026-02-18 19:08:38 -0800

    Fix migration for test compatibility

--- copi.owasp.org/priv/repo/migrations/20260218120000_add_unique_constraint_to_votes.exs
+++ copi.owasp.org/priv/repo/migrations/20260218120000_add_unique_constraint_to_votes.exs
@@ -3,28 +3,23 @@ defmodule Copi.Repo.Migrations.AddUniqueConstraintToVotes do
 
   def up do
     # Remove any existing duplicate votes first
-    # Use DO $$ block to handle case where table might be empty
+    # This runs only if votes table has  data
     execute """
-    DO $$
-    BEGIN
-      DELETE FROM votes a USING votes b
-      WHERE a.id > b.id 
-      AND a.player_id = b.player_id 
-      AND a.dealt_card_id = b.dealt_card_id;
-    EXCEPTION
-      WHEN others THEN NULL;
-    END $$;
+    DELETE FROM votes a 
+    WHERE a.id IN (
+      SELECT a2.id FROM votes a2
+      JOIN votes b ON a2.player_id = b.player_id AND a2.dealt_card_id = b.dealt_card_id
+      WHERE a2.id > b.id
+    )
     """
 
     # Create unique index
-    # Note: concurrently option requires @disable_ddl_transaction which
-    # can cause issues in test environments, so we use regular index creation
     create unique_index(:votes, [:player_id, :dealt_card_id], 
       name: :votes_player_dealt_card_unique_index)
   end
 
   def down do
-    drop index(:votes, [:player_id, :dealt_card_id],
+    drop_if_exists index(:votes, [:player_id, :dealt_card_id],
       name: :votes_player_dealt_card_unique_index)
   end
 end
