4e486e6809db8082be8391dd4f32a1c27a84b408
immortal71 <newaashish190@gmail.com>
2026-02-25 10:23:33 -0800

    Simplify tests - remove redundant transaction tests
    Removed duplicate tests that only covered success path already
    tested by existing 'successfully starts game with 3+ players' test.
    Also removed unnecessary Ecto.Query import.
    
    Signed-off-by: immortal71 <newaashish190@gmail.com>

--- copi.owasp.org/test/copi_web/live/game_live/show_test.exs
+++ copi.owasp.org/test/copi_web/live/game_live/show_test.exs
@@ -2,7 +2,6 @@ defmodule CopiWeb.GameLive.ShowTest do
   use CopiWeb.ConnCase, async: false
 
   import Phoenix.LiveViewTest
-  import Ecto.Query
 
   alias Copi.Cornucopia
 
@@ -85,78 +84,6 @@ defmodule CopiWeb.GameLive.ShowTest do
       # Verify started_at timestamp hasn't changed
       {:ok, updated_game} = Cornucopia.Game.find(started_game.id)
       assert DateTime.compare(updated_game.started_at, original_time) == :eq
-    end
-
-    test "transaction ensures atomicity - no partial card dealing on failure", %{conn: conn, game: game} do
-      # Add 3 players
-      {:ok, player1} = Cornucopia.create_player(%{name: "Player 1", game_id: game.id})
-      {:ok, player2} = Cornucopia.create_player(%{name: "Player 2", game_id: game.id})
-      {:ok, player3} = Cornucopia.create_player(%{name: "Player 3", game_id: game.id})
-
-      # Count initial dealt cards (should be 0)
-      initial_card_count = Copi.Repo.aggregate(Copi.Cornucopia.DealtCard, :count)
-
-      {:ok, view, _html} = live(conn, "/games/#{game.id}")
-
-      # Simulate a scenario where transaction would fail
-      # We'll use a mock that makes the game update fail in the transaction
-      # This tests that card dealing is rolled back when game update fails
-      
-      # For this test, we verify normal operation first
-      render_click(view, "start_game", %{})
-
-      # Verify cards were dealt (transaction succeeded)
-      {:ok, updated_game} = Cornucopia.Game.find(game.id)
-      assert updated_game.started_at != nil
-      
-      final_card_count = Copi.Repo.aggregate(Copi.Cornucopia.DealtCard, :count)
-      assert final_card_count > initial_card_count
-      
-      # Verify all players received cards
-      dealt_cards_player1 = Copi.Repo.all(
-        from dc in Copi.Cornucopia.DealtCard, where: dc.player_id == ^player1.id
-      )
-      dealt_cards_player2 = Copi.Repo.all(
-        from dc in Copi.Cornucopia.DealtCard, where: dc.player_id == ^player2.id
-      )
-      dealt_cards_player3 = Copi.Repo.all(
-        from dc in Copi.Cornucopia.DealtCard, where: dc.player_id == ^player3.id
-      )
-      
-      # All players should have cards (round-robin dealing)
-      assert length(dealt_cards_player1) > 0
-      assert length(dealt_cards_player2) > 0
-      assert length(dealt_cards_player3) > 0
-    end
-
-    test "transaction protection prevents database corruption", %{conn: conn, game: game} do
-      # Add 3 players
-      {:ok, _player1} = Cornucopia.create_player(%{name: "Player 1", game_id: game.id})
-      {:ok, _player2} = Cornucopia.create_player(%{name: "Player 2", game_id: game.id})
-      {:ok, _player3} = Cornucopia.create_player(%{name: "Player 3", game_id: game.id})
-
-      {:ok, view, _html} = live(conn, "/games/#{game.id}")
-
-      # Start the game
-      render_click(view, "start_game", %{})
-
-      # Verify transaction created consistent state
-      {:ok, updated_game} = Cornucopia.Game.find(game.id)
-      assert updated_game.started_at != nil
-
-      # Count total dealt cards
-      total_cards = Copi.Repo.aggregate(Copi.Cornucopia.DealtCard, :count)
-      
-      # Should have dealt all cards from the shuffled deck
-      # For webapp edition with 2 suits (hearts, clubs), we expect certain number of cards
-      assert total_cards > 0
-      
-      # Verify no orphaned cards without players
-      orphaned_cards = Copi.Repo.all(
-        from dc in Copi.Cornucopia.DealtCard,
-        where: is_nil(dc.player_id)
-      )
-      assert length(orphaned_cards) == 0
     end
   end
 end
