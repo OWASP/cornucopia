d15b8e094079f7c9af0bea642b723ff208ad83d8
Adarshkumar0509 <adarshkumarthakur0509@gmail.com>
2026-02-26 16:39:11 +0530

    fix: correct scrambled Elixir syntax in game and player form components

--- copi.owasp.org/lib/copi_web/live/game_live/form_component.ex
+++ copi.owasp.org/lib/copi_web/live/game_live/form_component.ex
@@ -1,7 +1,6 @@
 defmodule CopiWeb.GameLive.FormComponent do
   use CopiWeb, :live_component
   use Phoenix.Component
-
   alias Copi.Cornucopia
 
   @impl true
@@ -11,7 +10,6 @@ defmodule CopiWeb.GameLive.FormComponent do
       <.header1>
         <%= @title %>
       </.header1>
-
       <.simple_form
         for={@form}
         id="game-form"
@@ -20,19 +18,17 @@ defmodule CopiWeb.GameLive.FormComponent do
         phx-submit="save"
       >
         <.input field={@form[:name]} type="text" label={gettext("Game name")} />
-
+        <div class="border border-yellow-400 bg-yellow-50 text-yellow-800 rounded-md p-3 mt-2">
+          <p class="text-sm">
+            Please note: Avoid submitting sensitive and/or personal information to minimize the risk of data exfiltration due to accidental data exposure.
+          </p>
+        </div>
         <:actions>
           <.primary_button phx-disable-with="Saving..." class="m-auto block py-2 px-4">
             <%= gettext("Save game") %>
           </.primary_button>
         </:actions>
       </.simple_form>
-
-      <div class="border border-yellow-400 bg-yellow-50 text-yellow-800 rounded-md p-3 mt-4">
-        <p class="text-sm">
-          Please note: Avoid submitting sensitive and/or personal information to minimize the risk of data exfiltration due to accidental data exposure.
-        </p>
-      </div>
     </div>
     """
   end
@@ -50,6 +46,7 @@ defmodule CopiWeb.GameLive.FormComponent do
   @impl true
   def handle_event("validate", %{"game" => game_params}, socket) do
     changeset =
+      socket.assigns.game
       |> Cornucopia.change_game(game_params)
       |> Map.put(:action, :validate)
 
@@ -57,38 +54,37 @@ defmodule CopiWeb.GameLive.FormComponent do
   end
 
   @impl true
+  def handle_event("save", %{"game" => game_params}, socket) do
     save_game(socket, socket.assigns.action, game_params)
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
   end
 
+  defp save_game(socket, :edit, game_params) do
     case Cornucopia.update_game(socket.assigns.game, game_params) do
+      {:ok, _game} ->
         {:noreply,
          socket
+         |> put_flash(:info, "Game updated successfully")
          |> push_navigate(to: socket.assigns.return_to)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign_form(socket, changeset)}
     end
   end
+
   defp save_game(socket, :new, game_params) do
     case Cornucopia.create_game(game_params) do
       {:ok, game} ->
         {:noreply,
+         socket
          |> put_flash(:info, "Game created successfully")
          |> push_navigate(to: ~p"/games/#{game.id}")}
 
+      {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign_form(socket, changeset)}
     end
-end
   end
-      {:error, %Ecto.Changeset{} = changeset} ->
-         socket
-
-         |> put_flash(:info, "Game updated successfully")
-      {:ok, _game} ->
-  defp save_game(socket, :edit, game_params) do
-    assign(socket, :form, to_form(changeset))
-  def handle_event("save", %{"game" => game_params}, socket) do
-
+end
