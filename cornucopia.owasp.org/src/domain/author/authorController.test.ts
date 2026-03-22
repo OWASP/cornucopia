e8180a78e79b04d5b91dc48976ca00a91b0adcba
Adarsh Kumar <adarshkumarthakur0509@gmail.com>
2026-03-22 10:09:55 +0530

    test: perfectly restore deleted tests from master to recover 95% global coverage

--- cornucopia.owasp.org/src/domain/author/authorController.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import * as AuthorController from './authorController';
-
-describe('AuthorController Coverage', () => {
-    it('should exist and be defined', () => {
-        expect(AuthorController).toBeDefined();
-    });
-});
