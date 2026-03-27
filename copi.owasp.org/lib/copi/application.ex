0254f4c7a0b33973d4618602a9e34d289ab64f1d
Adarsh Kumar <adarshkumarthakur0509@gmail.com>
2026-03-28 00:58:23 +0530

    chore: clean up comments
    Signed-off-by: Adarsh Kumar <adarshkumarthakur0509@gmail.com>

--- copi.owasp.org/lib/copi/application.ex
+++ copi.owasp.org/lib/copi/application.ex
@@ -1,38 +1,24 @@
 defmodule Copi.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Ecto repository
       Copi.Repo,
-      # Start the Telemetry supervisor
       CopiWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: Copi.PubSub},
-      # Start the RateLimiter for IP-based rate limiting (CAPEC-212 protection)
       Copi.RateLimiter,
-      # Start the DNS clustering
       {DNSCluster, query: Application.get_env(:copi, :dns_cluster_query) || :ignore},
-      # Start the Endpoint (http/https)
       CopiWeb.Endpoint
-      # Start a worker by calling: Copi.Worker.start_link(arg)
-      # {Copi.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Copi.Supervisor]
     result = Supervisor.start_link(children, opts)
     Task.start(fn -> Copi.Encrypted.Migrator.run() end)
     result
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     CopiWeb.Endpoint.config_change(changed, removed)
     :ok
