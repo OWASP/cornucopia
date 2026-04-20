ca334666ab601a7b79f6adfcc2a16fb9f4e83fa3
immortal71 <newaashish190@gmail.com>
2026-04-20 06:50:50 +0545

    fix(copi): align conflicted controllers with upstream for stable coverage

--- copi.owasp.org/lib/copi_web/controllers/health_controller.ex
+++ copi.owasp.org/lib/copi_web/controllers/health_controller.ex
@@ -1,30 +1,15 @@
 defmodule CopiWeb.HealthController do
   use CopiWeb, :controller
-  require Logger
 
   def index(conn, _params) do
     # Check database connection with timeouts and exception handling
     try do
-      case Copi.Repo.query("SELECT 1", [], timeout: 1_000, pool_timeout: 1_000) do
-        {:ok, _} ->
-          send_resp(conn, :ok, "healthy\n")
-        # coveralls-ignore-start
-        {:error, _} ->
-          send_resp(conn, :service_unavailable, "not ready\n")
-        # coveralls-ignore-stop
-      end
+      {:ok, _} = Copi.Repo.query("SELECT 1", [], timeout: 1_000, pool_timeout: 1_000)
+      send_resp(conn, :ok, "healthy\n")
     rescue
-      # coveralls-ignore-start
-      e ->
-        Logger.error("Health check exception: #{inspect(e)}")
-        send_resp(conn, :service_unavailable, "not ready\n")
-      # coveralls-ignore-stop
+      _ -> send_resp(conn, :service_unavailable, "not ready\n")
     catch
-      # coveralls-ignore-start
-      :exit, reason ->
-        Logger.error("Health check exit: #{inspect(reason)}")
-        send_resp(conn, :service_unavailable, "not ready\n")
-      # coveralls-ignore-stop
+      _, _ -> send_resp(conn, :service_unavailable, "not ready\n")
     end
   end
 end
