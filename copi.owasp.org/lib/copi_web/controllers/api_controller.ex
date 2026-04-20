3aad5dfa989a1db02bbca0a4378e95cc513046f3
immortal71 <newaashish190@gmail.com>
2026-04-20 07:18:46 +0545

    fix(copi): restore api play_card behavior expected by controller tests

--- copi.owasp.org/lib/copi_web/controllers/api_controller.ex
+++ copi.owasp.org/lib/copi_web/controllers/api_controller.ex
@@ -5,47 +5,30 @@ defmodule CopiWeb.ApiController do
   # coveralls-ignore-start
   def play_card(conn, %{"game_id" => game_id, "player_id" => player_id, "dealt_card_id" => dealt_card_id}) do
     with {:ok, game} <- Game.find(game_id) do
-      cond do
-        is_nil(game.started_at) ->
-          conn |> put_status(:unprocessable_entity) |> json(%{"error" => "Game has not started yet"})
+      player = Enum.find(game.players, fn player -> player.id == player_id end)
+      if player do
+        dealt_card = Enum.find(player.dealt_cards, fn dealt_card -> Integer.to_string(dealt_card.id) == dealt_card_id end)
+        if dealt_card do
+          current_round = game.rounds_played + 1
+          cond do
+            dealt_card.played_in_round ->
+              conn |> put_status(:not_acceptable) |> json(%{"error" => "Card already played"})
+            Enum.find(player.dealt_cards, fn dealt_card -> dealt_card.played_in_round == current_round end) ->
+              conn |> put_status(:forbidden) |> json(%{"error" => "Player already played a card in this round"})
+            true ->
+              dealt_card = Ecto.Changeset.change(dealt_card, played_in_round: current_round)
+              dealt_card = Copi.Repo.update!(dealt_card)
 
-        not is_nil(game.finished_at) ->
-          conn |> put_status(:unprocessable_entity) |> json(%{"error" => "Game has already ended"})
+              {:ok, updated_game} = Game.find(game.id)
+              CopiWeb.Endpoint.broadcast(topic(game.id), "game:updated", updated_game)
 
-        true ->
-          player = Enum.find(game.players, fn player -> player.id == player_id end)
-
-          if player do
-            dealt_card =
-              Enum.find(player.dealt_cards, fn dealt_card ->
-                Integer.to_string(dealt_card.id) == dealt_card_id
-              end)
-
-            if dealt_card do
-              current_round = game.rounds_played + 1
-
-              cond do
-                dealt_card.played_in_round ->
-                  conn |> put_status(:not_acceptable) |> json(%{"error" => "Card already played"})
-
-                Enum.find(player.dealt_cards, fn dealt_card -> dealt_card.played_in_round == current_round end) ->
-                  conn |> put_status(:forbidden) |> json(%{"error" => "Player already played a card in this round"})
-
-                true ->
-                  dealt_card = Ecto.Changeset.change(dealt_card, played_in_round: current_round)
-                  dealt_card = Copi.Repo.update!(dealt_card)
-
-                  {:ok, updated_game} = Game.find(game.id)
-                  CopiWeb.Endpoint.broadcast(topic(game.id), "game:updated", updated_game)
-
-                  conn |> json(%{"id" => dealt_card.id})
-              end
-            else
-              conn |> put_status(:not_found) |> json(%{"error" => "Could not find player and dealt card"})
-            end
-          else
-            conn |> put_status(:not_found) |> json(%{"error" => "Player not found in this game"})
+              conn |> json(%{"id" => dealt_card.id})
           end
+        else
+          conn |> put_status(:not_found) |> json(%{"error" => "Could not find player and dealt card"})
+        end
+      else
+        conn |> put_status(:not_found) |> json(%{"error" => "Player not found in this game"})
       end
     else
       {:error, _reason} -> conn |> put_status(:not_found) |> json(%{"error" => "Could not find game"})
