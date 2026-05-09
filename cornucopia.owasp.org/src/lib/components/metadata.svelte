851ea75acad8ebc11e01aab64d2b5bdd47ad3368
10-trix <ishutan223@gmail.com>
2026-05-09 10:51:29 +0530

    fix: remove duplicate title/description from global meta data to avoid SSR conflicts.

--- cornucopia.owasp.org/src/lib/components/metadata.svelte
+++ cornucopia.owasp.org/src/lib/components/metadata.svelte
@@ -2,22 +2,12 @@
     <!-- Facebook Meta Tags -->
     <meta property="og:url" content="https://cornucopia.owasp.org/" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="OWASP Cornucopia" />
-    <meta
-        property="og:description"
-        content="A threat modelling card game to help development teams identify security requirements."
-    />
     <meta property="og:image" content="/images/opengraph.png" />
 
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image" />
     <meta property="twitter:domain" content="cornucopia.owasp.org" />
     <meta property="twitter:url" content="https://cornucopia.owasp.org/" />
-    <meta name="twitter:title" content="OWASP Cornucopia" />
-    <meta
-        name="twitter:description"
-        content="A threat modelling card game to help development teams identify security requirements."
-    />
     <meta name="twitter:image" content="/images/opengraph.png" />
     <!-- END of Tags -->
 </svelte:head>
