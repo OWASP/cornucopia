c3a4d2552f043ee0e0b9fc207276889e013b61d5
Adarsh Kumar <adarshkumarthakur0509@gmail.com>
2026-06-10 17:13:42 +0530

    Fix: use single change migration and truncate long misc values in YAML

--- copi.owasp.org/priv/repo/migrations/20260710100557_populate_dbd_cards.exs
+++ copi.owasp.org/priv/repo/migrations/20260710100557_populate_dbd_cards.exs
@@ -3,13 +3,7 @@ defmodule Copi.Repo.Migrations.PopulateDbdCards do
 
   import Copi.CardMigration
 
-  @cards_file Path.join(:code.priv_dir(:copi), "repo/cornucopia/dbd-cards-1.0-en.yaml")
-
-  def up do
-    add_cards_to_database(@cards_file, nil)
-  end
-
-  def down do
-    raise "Irreversible rollback: implement deletion for cards loaded from #{@cards_file}"
+  def change do
+    add_cards_to_database(Path.join(:code.priv_dir(:copi), "/repo/cornucopia/dbd-cards-1.0-en.yaml"), nil)
   end
 end
