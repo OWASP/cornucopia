0526307fc64b66a6aa0bd5fe388f9f278af6173a
Ayman Algamal <ayman01287985950@gmail.com>
2026-07-17 16:24:11 +0300

    Extracted the duplicated card id allowlist into a shared cardIds module for mantainability

--- cornucopia.owasp.org/src/routes/edition/[edition]/[card]/+page.svelte
+++ cornucopia.owasp.org/src/routes/edition/[edition]/[card]/+page.svelte
@@ -3,6 +3,7 @@
   import CardFound from "$lib/components/cardFound.svelte";
   import CardNotFound from "$lib/components/cardNotFound.svelte";
   import type { Card } from "$domain/card/card";
+  import { isKnownCardId } from "$domain/card/cardIds";
   import { readLang, readTranslation } from "$lib/stores/stores";
   import type { PageData } from './$types';
 
@@ -16,36 +17,9 @@
 
   const languages = $derived(data.languages);
 
-  function cardFound() 
+  function cardFound()
     {
-    let cards_options = [
-      "AAA","AA2","AA3","AA4","AA5","AA6","AA7","AA8","AA9","AAX","AAJ","AAQ","AAK",
-      "ATA","AT2","AT3","AT4","AT5","AT6","AT7","AT8","AT9","ATX","ATJ","ATQ","ATK",
-      "AZA","AZ2","AZ3","AZ4","AZ5","AZ6","AZ7","AZ8","AZ9","AZX","AZJ","AZQ","AZK",
-      "CA","C2","C3","C4","C5","C6","C7","C8","C9","CX","CJ","CQ","CK",
-      "CMA","CM2","CM3","CM4","CM5","CM6","CM7","CM8","CM9","CMX","CMJ","CMQ","CMK",
-      "CRA","CR2","CR3","CR4","CR5","CR6","CR7","CR8","CR9","CRX","CRJ","CRQ","CRK",
-      "CRMA","CRM2","CRM3","CRM4","CRM5","CRM6","CRM7","CRM8","CRM9","CRMX","CRMJ","CRMQ","CRMK",
-      "NSA","NS2","NS3","NS4","NS5","NS6","NS7","NS8","NS9","NSX","NSJ","NSQ","NSK",
-      "PCA","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PCX","PCJ","PCQ","PCK",
-      "RSA","RS2","RS3","RS4","RS5","RS6","RS7","RS8","RS9","RSX","RSJ","RSQ","RSK",
-      "SMA","SM2","SM3","SM4","SM5","SM6","SM7","SM8","SM9","SMX","SMJ","SMQ","SMK",
-      "VEA","VE2","VE3","VE4","VE5","VE6","VE7","VE8","VE9","VEX","VEJ","VEQ","VEK",
-      "LLMA","LLM2","LLM3","LLM4","LLM5","LLM6","LLM7","LLM8","LLM9","LLMX","LLMJ","LLMQ","LLMK",
-      "AAIA","AAI2","AAI3","AAI4","AAI5","AAI6","AAI7","AAI8","AAI9","AAIX","AAIJ","AAIQ","AAIK",
-      "CLDA","CLD2","CLD3","CLD4","CLD5","CLD6","CLD7","CLD8","CLD9","CLDX","CLDJ","CLDQ","CLDK",
-      "FREA","FRE2","FRE3","FRE4","FRE5","FRE6","FRE7","FRE8","FRE9","FREX","FREJ","FREQ","FREK",
-      "DVOA","DVO2","DVO3","DVO4","DVO5","DVO6","DVO7","DVO8","DVO9","DVOX","DVOJ","DVOQ","DVOK",
-      "SEA","SE2","SE3","SE4","SE5","SE6","SE7","SE8","SE9","SEX","SEJ","SEQ","SEK",
-      "BOTA","BOT2","BOT3","BOT4","BOT5","BOT6","BOT7","BOT8","BOT9","BOTX","BOTJ","BOTQ","BOTK",
-      "SPA","SP2","SP3","SP4","SP5","SP6","SP7","SP8","SP9","SPX","SPJ","SPQ","SPK",
-      "TAA","TA2","TA3","TA4","TA5","TA6","TA7","TA8","TA9","TAX","TAJ","TAQ","TAK",
-      "REA","RE2","RE3","RE4","RE5","RE6","RE7","RE8","RE9","REX","REJ","REQ","REK",
-      "IDA","ID2","ID3","ID4","ID5","ID6","ID7","ID8","ID9","IDX","IDJ","IDQ","IDK",
-      "DSA","DS2","DS3","DS4","DS5","DS6","DS7","DS8","DS9","DSX","DSJ","DSQ","DSK",
-      "EPA","EP2","EP3","EP4","EP5","EP6","EP7","EP8","EP9","EPX","EPJ","EPQ","EPK",
-      "JOA","JOB","JOAM","JOBM","CORNUCOPIA"]
-    return (cards_options.includes(String(card?.id).toUpperCase()))
+    return isKnownCardId(card?.id)
   }
 
   function getEdition(str: string) : string {
