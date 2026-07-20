40b55ce96f468b07e20dc62b2e2b16357505d98f
Uncle Joe <1244005+sydseter@users.noreply.github.com>
2026-07-20 21:04:13 +0200

    Update headers-stage.js

--- cornucopia.owasp.org/script/headers-stage.js
+++ cornucopia.owasp.org/script/headers-stage.js
@@ -132,6 +132,16 @@ function main() {
   Access-Control-Allow-Origin: *
   ! Content-Type
   Content-Type: application/json
+/api/cre/dbd/en
+  ! Access-Control-Allow-Origin
+  Access-Control-Allow-Origin: *
+  ! Content-Type
+  Content-Type: application/json
+/api/lang/dbd/1.0
+  ! Access-Control-Allow-Origin
+  Access-Control-Allow-Origin: *
+  ! Content-Type
+  Content-Type: application/json
 /api/cre/eop/en
   ! Access-Control-Allow-Origin
   Access-Control-Allow-Origin: *
